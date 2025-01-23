6d3492592662328f27e6b6492986e3d485fa8c7f
Khanh <nbk456123@gmail.com>
2025-01-23 22:59:00 +0700

    add hooks for genre conversion, movie playback, and updating liked/watched/my list

---
+++ frontend/hooks/useGenresConverter.jsx
@@ -0,0 +1,20 @@
+import { genresList } from "../constants/constance";
+
+const useGenreConverter = () => {
+  const convertGenre = (genreIds) => {
+    const genresConvertedList = [];
+    genreIds
+      .slice(0, 3)
+      .map((genreId) =>
+        genresList
+          .filter((el) => el.id === genreId)
+          .map((el) => genresConvertedList.push(el.name))
+      );
+
+    return genresConvertedList;
+  };
+
+  return { convertGenre };
+};
+
+export default useGenreConverter;
