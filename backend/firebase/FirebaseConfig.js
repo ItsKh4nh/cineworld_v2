46bc0cfe23486e1beb3952d5e37ce2a143fe6341
Khanh <nbk456123@gmail.com>
2025-01-23 23:12:18 +0700

    add Firebase configuration for app initialization

--- backend/firebase/FirebaseConfig.js
+++ backend/firebase/FirebaseConfig.js
@@ -2,7 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyC0ZJyNT-OgT2F4M3au3VcaneqUjJ8t9JQ",
+  authDomain: "cineworld-06166.firebaseapp.com",
+  projectId: "cineworld-06166",
+  storageBucket: "cineworld-06166.firebasestorage.app",
+  messagingSenderId: "735024717951",
+  appId: "1:735024717951:web:c4250e008c525c03ee9f4a",
+  measurementId: "G-4ECR63D18T",
+};
 
 //Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
