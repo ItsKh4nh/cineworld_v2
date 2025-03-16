3b85bf88d2ebe63c5b58da0dc3fdac9febc79467
Khanh <nbk456123@gmail.com>
2025-03-16 17:15:55 +0700

    feat: Update RowPost component with improved list interaction and styling; add new file patterns to .gitignore

--- src/components/RowPost/RowPost.jsx
+++ src/components/RowPost/RowPost.jsx
@@ -157,26 +157,59 @@ function RowPost(props) {
 
                         {props.movieData != null ? (
                           <>
-                            <div
-                              onMouseEnter={() => setShouldPop(false)}
-                              onMouseLeave={() => setShouldPop(true)}
-                              className="text-white w-9 h-9 border-[2px] rounded-full flex items-center justify-center mr-1 backdrop-blur-[1px] shadow-md ease-linear transition-all duration-150 hover:text-black hover:bg-white"
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-5 h-5"
+                            {isInMyList ? (
+                              <div
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  if (openRatingModal) {
+                                    openRatingModal(obj, User);
+                                  }
+                                }}
+                                onMouseEnter={() => setShouldPop(false)}
+                                onMouseLeave={() => setShouldPop(true)}
+                                className="bg-cineworldYellow text-white w-9 h-9 rounded-full flex items-center justify-center mr-1 backdrop-blur-[1px] shadow-md ease-linear transition-all duration-150 hover:bg-white hover:text-cineworldYellow"
                               >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M19.5 12h-15"
-                                />
-                              </svg>
-                            </div>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="w-5 h-5"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                                  />
+                                </svg>
+                              </div>
+                            ) : (
+                              <div
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  addToMyList({...obj, isInMyList: false});
+                                }}
+                                onMouseEnter={() => setShouldPop(false)}
+                                onMouseLeave={() => setShouldPop(true)}
+                                className="text-white w-9 h-9 border-[2px] rounded-full flex items-center justify-center mr-1 backdrop-blur-[1px] shadow-md ease-linear transition-all duration-150 hover:text-black hover:bg-white"
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="w-5 h-5"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 4.5v15m7.5-7.5h-15"
+                                  />
+                                </svg>
+                              </div>
+                            )}
                           </>
                         ) : (
                           <>
